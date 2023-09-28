642bdb83431402ddc561fcb4b2700d02505d67e1
Emaan Zaffar <emaan100n@gmail.com>
2023-09-28 16:26:58 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
+
+import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
+
+export const metadata: Metadata = {
+  title: 'Airbnb Clone',
+  description: 'Airbnb Clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
